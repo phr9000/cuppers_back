f02419fa3c929c37f0c47f0b08d4bf78a27d41a9
studentinbb <75927514+studentinbb@users.noreply.github.com>
2022-06-16 22:13:20 +0800

    [Update]cafelist, cafedetail

--- mysql/sql.js
+++ mysql/sql.js
@@ -1,5 +1,5 @@
 module.exports = {
-  cafeList: `SELECT *, 
+  cafeList: `SELECT t1.cafe_id, t1.cafe_name_pr, t1.cafe_address, t1.cafe_region, t1.cafe_latitude, t1.cafe_longitude, t1.cafe_description, t1.cafe_type, t1.cafe_img, 
   (
     SELECT COUNT(*) 
     FROM user_cafe_likeit t2
@@ -10,31 +10,42 @@ module.exports = {
     FROM review t3
     WHERE t3.cafe_id = t1.cafe_id
     ) as reviews
-    From cafe t1`,
-  cafeDetail: `SELECT * FROM cafe WHERE cafe_id = ?`,
+  FROM cafe t1
+  LIMIT 10`,
+  cafeListKeyword: `SELECT t1.cafe_id, t3.keyword_id, t3.keyword_name
+  FROM cafe t1, cafe_keyword t2, keyword t3
+  WHERE t1.cafe_id = t2.cafe_id and t2.keyword_id = t3.keyword_id`,
+  cafeListOpTime: `SELECT t2.cafe_id, t2.operation_day as day, t2.operation_time as time
+  FROM cafe t1, cafe_operation_time t2
+  WHERE t1.cafe_id = t2.cafe_id`,
+  cafeDetail: `SELECT *,
+  (
+    SELECT COUNT(*) 
+    FROM user_cafe_likeit t2
+    WHERE t2.cafe_id = t1.cafe_id
+    ) as likeit
+  FROM cafe t1
+  WHERE cafe_id = ?`,
   cafeDelete: ``,
-  cafeImage: `SELECT t2.type, t2.cafe_image_url
+  cafeImage: `SELECT t2.*
   FROM cafe t1, images_cafe t2
   WHERE t1.cafe_id = ? and t1.cafe_id = t2.cafe_id`,
-  cafeBrewingOption: `SELECT t3.brewing_name, t3.brewing_icon 
+  cafeBrewingOption: `SELECT t3.brewing_name as name, t3.brewing_icon as icon 
   FROM cafe t1, cafe_brewing_option t2, brewing_option t3
   WHERE t1.cafe_id = ? and t1.cafe_id = t2.cafe_id and t2.brewing_option_id = t3.brewing_option_id`,
-  cafeOperationTime: `SELECT t2.operation_day, t2.operation_time
+  cafeOpTime: `SELECT t2.operation_day as day, t2.operation_time as time
   FROM cafe t1, cafe_operation_time t2
   WHERE t1.cafe_id = ? and t1.cafe_id = t2.cafe_id`,
   // 활성 조건 추가해야 함
-  cafeKeyword: `SELECT t3.keyword_name, t3.keyword_icon
+  cafeKeyword: `SELECT t3.keyword_name as name, t3.keyword_icon as icon
   FROM cafe t1, cafe_keyword t2, keyword t3
   WHERE t1.cafe_id = ? and t1.cafe_id = t2.cafe_id and t2.keyword_id = t3.keyword_id`,
   cafeMenu: `SELECT t2.*
   FROM cafe t1, menu t2
   WHERE t1.cafe_id = ? and t1.cafe_id = t2.cafe_id`,
-  cafeFacility: `SELECT t3.facility_name, t3.facility_icon, t3.facility_type
+  cafeFacility: `SELECT t3.facility_name as name, t3.facility_icon as icon, t3.facility_type as type
   FROM cafe t1, cafe_facility t2, facility t3
   WHERE t1.cafe_id = ? and t1.cafe_id = t2.cafe_id and t2.facility_id = t3.facility_id`,
-  cafeLikeIt: `SELECT COUNT(*) as count
-  FROM user_cafe_likeit
-  WHERE cafe_id = ?`,
   cafeReview: `SELECT t1.*, t2.user_nickname, t3.images_review_url as thumbnail_url,
   (
     SELECT COUNT(*) 
--- routes/cafe.js
+++ routes/cafe.js
@@ -2,39 +2,68 @@ const express = require("express");
 const router = express.Router();
 const mysql = require("../mysql");
 
-// [조회] cafe 테이블의 모든 데이터
+// [조회] cafe 리스트
 router.get("/", async (req, res) => {
   const cafeList = await mysql.query("cafeList");
+  // 카페 테이블의 데이터 불러오는 쿼리(LIMIT 10)
+
+  const cafeListKeyword = await mysql.query("cafeListKeyword");
+  // 카페 테이블 데이터와 연결된 다른 테이블의 데이터 불러오는 쿼리 (아래는 쿼리문)
+  // cafeListKeyword: `SELECT t1.cafe_id, t3.keyword_id, t3.keyword_name
+  // FROM cafe t1, cafe_keyword t2, keyword t3
+  // WHERE t1.cafe_id = t2.cafe_id and t2.keyword_id = t3.keyword_id`
+
+  // -> 연결된 테이블 정보 가져오는 두번째 쿼리는 LIMIT을 걸 수 없음
+  // (카페 데이터 하나 당 몇 개의 키워드 데이터가 join되었는지 모름)
+
+  const cafeListOpTime = await mysql.query("cafeListOpTime");
+
+  cafeList.forEach((cafe) => {
+    const temp_id = cafe.cafe_id;
+    cafe.cafe_keyword = [];
+    cafeListKeyword
+      .filter((obj) => obj.cafe_id === temp_id)
+      .forEach((keyword) => {
+        cafe.cafe_keyword.push(keyword);
+      });
+  });
+  // 카페 object 내에 키워드 object들을 배열로 추가
+  // for문을 두 번 돌리는 형태로 진행
+
+  cafeList.forEach((cafe) => {
+    const temp_id = cafe.cafe_id;
+    cafe.cafe_opTime = [];
+    cafeListOpTime
+      .filter((obj) => obj.cafe_id === temp_id)
+      .forEach((opTime) => {
+        cafe.cafe_opTime.push(opTime);
+      });
+  });
+
   res.send(cafeList);
 });
 
-// [조회] front가 요청한 cafe_id의 cafe 상세 정보
+// [조회] cafe 상세페이지1 - cafe 기본 정보
 router.get("/:cafe_id", async (req, res) => {
   const { cafe_id } = req.params;
   // cafe 테이블의 데이터 호출
   const cafeDetail = await mysql.query("cafeDetail", cafe_id);
 
   // 연결된 테이블들의 데이터를 받아서 배열로 object에 삽입
-  const images = [];
   const brewingOptions = [];
-  const operationTimes = [];
+  const opTimes = [];
   const keywords = [];
   const menus = [];
   const facilities = [];
-
-  const cafeImage = await mysql.query("cafeImage", cafe_id);
-  cafeImage.forEach((key) => {
-    images.push(key);
-  });
 
   const cafeBrewingOption = await mysql.query("cafeBrewingOption", cafe_id);
   cafeBrewingOption.forEach((key) => {
     brewingOptions.push(key);
   });
 
-  const cafeOperationTime = await mysql.query("cafeOperationTime", cafe_id);
-  cafeOperationTime.forEach((key) => {
-    operationTimes.push(key);
+  const cafeOpTime = await mysql.query("cafeOpTime", cafe_id);
+  cafeOpTime.forEach((key) => {
+    opTimes.push(key);
   });
 
   const cafeKeyword = await mysql.query("cafeKeyword", cafe_id);
@@ -52,17 +81,21 @@ router.get("/:cafe_id", async (req, res) => {
     facilities.push(key);
   });
 
-  cafeDetail[0].image = images;
   cafeDetail[0].brewingOption = brewingOptions;
-  cafeDetail[0].operationTime = operationTimes;
+  cafeDetail[0].opTime = opTimes;
   cafeDetail[0].keyword = keywords;
   cafeDetail[0].menu = menus;
   cafeDetail[0].facility = facilities;
 
-  const likeIt = await mysql.query("cafeLikeIt", cafe_id);
-  cafeDetail[0].likeIt = likeIt[0];
-
   res.send(cafeDetail[0]);
+});
+
+// [조회] cafe 상세페이지2 - cafe images
+router.get("/image/:cafe_id", async (req, res) => {
+  const { cafe_id } = req.params;
+  const cafeImage = await mysql.query("cafeImage", cafe_id);
+
+  res.send(cafeImage);
 });
 
 // router.get("/update", async (req, res) => {
