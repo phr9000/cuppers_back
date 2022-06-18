e6fe905c766c411725877f626ed1164011f31604
hailan <78142246+phr9000@users.noreply.github.com>
2022-06-18 09:07:41 +0900

    [기타] app.js express json 삭제
    [기타] app.js express json 삭제

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
-
-app.post("/", function (req, res) {
-  console.log(req.body); // 사용자의 JSON 요청
-  res.send(req.body); // JSON 응답
-});
-
 require("dotenv").config({ path: "mysql/.env" });
 const mysql = require("./mysql");
 
