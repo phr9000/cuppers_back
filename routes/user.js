a9076833f0855f2857c7cda28026fa8263267ae3
hailan <78142246+phr9000@users.noreply.github.com>
2022-06-22 19:26:22 +0900

    [test] 프론트 서버 연결되는지 테스트중

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,13 @@ const mysql = require("../mysql");
 // [조회] 사용자리스트 조회 api
 router.get("/", async (req, res) => {
   const userList = await mysql.query("userList");
-  res.send(userList);
+  if (userList) {
+    res.status(200).json(userList);
+  } else {
+    res
+      .status(404)
+      .json({ message: `서버는 연결되었는데 뭔가가 잘 못 되었다.` });
+  }
 });
 
 // [생성] 사용자리스트 생성 api
